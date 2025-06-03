d0abcfc2ede90ea2fc1be076bd59552a5e008c23
Piotr Siemieniak <siemieniak.piotr1@gmail.com>
2025-06-03 21:53:31 +0200

    Type fix

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -2,7 +2,11 @@ import { getDatafeed } from "@/lib/getDatafeed";
 import ClientPage from "@/views/ClientPage/ClientPage";
 import { notFound } from "next/navigation";
 
-export default async function Page({ params }: { params: { name: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ name: string }>;
+}) {
   const { name } = await params;
   const decodedName = decodeURIComponent(name);
   const response = await getDatafeed(decodedName);
